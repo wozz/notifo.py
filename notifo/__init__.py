563534e43aac6fcee86a807eb70fbeb90caf1cfc
wozz <wozz@koh.ms>
2010-09-09 01:24:07 +0400

    changed from kwargs to normal args

--- notifo/__init__.py
+++ notifo/__init__.py
@@ -3,14 +3,14 @@
 from notifo import Notifo
 
 __author__ = "Daniel Schauenberg"
-__version__ = "0.2.1"
+__version__ = "0.2.3"
 __license__ = "MIT"
 
-def send_message(login, pw, **kwargs):
-    return Notifo(login, pw).send_message(**kwargs)
+def send_message(login, pw, msg=None):
+    return Notifo(login, pw).send_message(msg)
 
-def send_notification(login, pw, **kwargs):
-    return Notifo(login, pw).send_notification(**kwargs)
+def send_notification(login, pw, to=None, msg=None, label=None, title=None, uri=None):
+    return Notifo(login, pw).send_notification(to,msg,label,title,uri)
 
 def subscribe_user(login, pw, user):
     return Notifo(login, pw).subscribe_user(user)
